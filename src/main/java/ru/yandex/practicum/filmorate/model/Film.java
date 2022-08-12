706a6fa8b564ae17fbdc6ccfa26f8a0b2acc7d52
vv1111vv <vv1111vv@yandex.ru>
2022-08-12 14:51:01 +0300

    Отправка ТЗ-9, ветка controllers-films-users   №1

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Film {
+    private long id;
+    @NotBlank
+    private String name;   //название не может быть пустым;
+    @Length(max = 200)
+    private String description;   //максимальная длина описания — 200 символов;
+    private LocalDate releaseDate;   // дата релиза — не раньше 28 декабря 1895 года;
+    @Positive
+    private int duration;   //  продолжительность фильма должна быть положительной.
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundObjectException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
+@Slf4j
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @Override
+    public Film create(Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film update(Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new NotFoundObjectException("Фильма с id " + film.getId() + " нет");
+        } else {
+            films.put(film.getId(), film);
+            return film;
+        }
+    }
+
+    @Override
+    public ArrayList<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
+}
